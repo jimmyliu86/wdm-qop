2bb8735c6a220ee0f1d5362b8e338bc997022fa4
aamadeo@gmail.com <aamadeo@gmail.com@ceb8b240-2948-c4dd-a6a1-f90e7d6bc26b>
2012-12-07 15:17:14 +0000

    Desarrollo completo v1 de la clase Nodo

--- WDM_QoP/WDM_QoP/src/wdm/Nodo.java
+++ WDM_QoP/WDM_QoP/src/wdm/Nodo.java
@@ -2,6 +2,7 @@ package wdm;
 
 public class Nodo {
 	private String label = "";
+	private boolean usaConversor = false;
 
 	public String getLabel() {
 		return label;
@@ -9,5 +10,17 @@ public class Nodo {
 
 	public void setLabel(String label) {
 		this.label = label;
+	}
+
+	public boolean usaConversor() {
+		return usaConversor;
+	}
+
+	public void utilizarConversor() {
+		this.usaConversor = true;
+	}
+	
+	public void inicializar(){
+		this.usaConversor = false;
 	}
 }
