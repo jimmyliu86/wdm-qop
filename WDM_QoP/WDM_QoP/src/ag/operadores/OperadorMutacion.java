f9eb607e391acac170da7b74b9e6a1e426d90044
rodas.marcelo@gmail.com <rodas.marcelo@gmail.com@ceb8b240-2948-c4dd-a6a1-f90e7d6bc26b>
2012-12-24 20:06:54 +0000

    La implementación no cuenta con operador de mutación, por ende no tiene implementación

--- WDM_QoP/WDM_QoP/src/ag/operadores/OperadorMutacion.java
+++ WDM_QoP/WDM_QoP/src/ag/operadores/OperadorMutacion.java
@@ -4,6 +4,10 @@ import ag.Individuo;
 
 /**
  * Interfaz que define la operacion de mutación.
+ * <p>
+ * OBSERVACION: No se utiliza un operador de mutación, debido a las
+ * características aleatorias del cruce implementado.
+ * </p>
  * 
  * @author mrodas
  * 
