350a67af02e860be0faf091e3a01217ada81e8e0
rodas.marcelo@gmail.com <rodas.marcelo@gmail.com@ceb8b240-2948-c4dd-a6a1-f90e7d6bc26b>
2012-12-29 17:12:17 +0000

    Se corrige y completa el cruce (falta asignar longitudes de onda)

--- WDM_QoP/WDM_QoP/src/ag/operadores/impl/MiCruce.java
+++ WDM_QoP/WDM_QoP/src/ag/operadores/impl/MiCruce.java
@@ -10,11 +10,30 @@ import ag.Gen;
 import ag.Solucion;
 import ag.operadores.OperadorCruce;
 
+/**
+ * Implementación específica del Cruce mencionado en paper de cnunez.
+ * <p>
+ * Este cruce se define asi: dada dos soluciones de entrada (s1 y s2), en una
+ * primera parte se copian los Caminos que son iguales entre los genes de los
+ * padres.
+ * </p>
+ * <p>
+ * Luego, en una segunda parte, se utiliza el algoritmo SPD para completar los
+ * caminos faltantes.
+ * </p>
+ * <p>
+ * Finalmente, en una tercera parte se asignan las longitudes de onda, la
+ * primera de forma aleatoria, y luego se trata de mantener dicha longitud de
+ * onda. En el caso que no se encuentra disponible la longitud de onda se
+ * procederá a elegir otro randomicamente. Así sucecivamente hasta llegar al
+ * final del camino.
+ * </p>
+ */
 public class MiCruce implements OperadorCruce {
 
-	// TODO Implementar Metodo especifico de cruce.
 	@Override
 	public Solucion cruzar(Solucion s1, Solucion s2) {
+
 		// Comprobamos que las solicitudes sea las mismas
 		if (!s1.mismasSolicitudes(s2))
 			throw new Error("Las solicitudes no coinciden");
@@ -24,7 +43,10 @@ public class MiCruce implements OperadorCruce {
 		Gen gen2 = new Gen();
 		ArrayList<CanalOptico> auxiliar = null;
 		ArrayList<CanalOptico> nuevoPrimario = null;
-		// se copian todos los genes cuyos caminos son iguales
+		ArrayList<CanalOptico> nuevoSecundario = null;
+		/*
+		 * Cálculos del Nuevo Camino Primario y del Nuevo Camino Secundario.
+		 */
 		for (int i = 0; i < s1.getGenes().size(); i++) {
 			gen1 = s1.getGenes().get(i);
 			gen2 = s2.getGenes().get(i);
@@ -32,54 +54,86 @@ public class MiCruce implements OperadorCruce {
 			ArrayList<CanalOptico> primarioGen2 = gen2.getPrimario();
 
 			auxiliar = new ArrayList<CanalOptico>(primarioGen1.size());
-			// Se recorre el primario m�s corto
+			// Se recorre el primario más corto
 			if (primarioGen1.size() <= primarioGen2.size()) {
 
 				for (CanalOptico co : primarioGen1) {
-					if (primarioGen2.contains(co)) {
+					if (primarioGen2.contains(co))
 						auxiliar.add(co);
-					} else {
+					else
 						auxiliar.add(null);
-					}
 				}
+
 			} else {
 				for (CanalOptico co : primarioGen2) {
-					if (primarioGen1.contains(co)) {
+					if (primarioGen1.contains(co))
 						auxiliar.add(co);
-					} else {
+					else
 						auxiliar.add(null);
-					}
 				}
 			}
-		}
 
-		// se carga el nuevo Camino Primario.
-		nuevoPrimario = new ArrayList<CanalOptico>(auxiliar.size());
-		Nodo inicio = null;
-		Nodo fin = null;
+			// se carga el nuevo Camino Primario.
+			nuevoPrimario = new ArrayList<CanalOptico>(auxiliar.size());
+			nuevoSecundario = new ArrayList<CanalOptico>(auxiliar.size());
+			Nodo inicio = null;
+			Nodo fin = null;
 
-		for (int j = 0; j < auxiliar.size(); j++) {
-			if (auxiliar.get(j) == null) {
-				if (j > 0) {
-					inicio = auxiliar.get(j - 1).getExtremoB();
+			for (int j = 0; j < auxiliar.size(); j++) {
+				/*
+				 * Caso null: los genes con valor null son los que se deben
+				 * sustituir por nuevos caminos (a través de SPD).
+				 */
+				if (auxiliar.get(j) == null) {
+					/*
+					 * Primero: Se debe obtener el Nodo inicio y el Nodo fin
+					 */
+					if (j > 0)
+						inicio = auxiliar.get(j - 1).getExtremoB();
+					else
+						inicio = s1.getSolicitudes().get(j).getOrigen();
+
+					while (auxiliar.get(j) == null) {
+						j++;
+					}
+					// Nodo Fin
+					fin = auxiliar.get(j).getExtremoA();
+
+					/*
+					 * Segundo: Realizar algoritmo Shortest Path Disjktra (SPD)
+					 * desde el Nodo inicio al Nodo fin.
+					 */
+					Camino subCamino = inicio.dijkstra(fin);
+					// Se agrega el camino encontrado al Nuevo Camino Primario
+					for (Salto salto : subCamino.getSaltos()) {
+						nuevoPrimario.add(salto.getCanal());
+					}
+
 				} else {
-					inicio = s1.getSolicitudes().get(j).getOrigen();
+					/*
+					 * Caso not null: los genes not null son iguales entre los
+					 * padres y por ende se agregan directamente.
+					 */
+					nuevoPrimario.add(auxiliar.get(j));
 				}
-
-				while (auxiliar.get(j) == null) {
-					j++;
-				}
-				fin = auxiliar.get(j).getExtremoA();
-				// Realizar algoritmo Shortest Path Disjktra (SPD) para restante
-				Camino caminito = inicio.dijkstra(fin);
-				// Se agrega el camino encontrado
-				for (Salto s : caminito.getSaltos()) {
-					nuevoPrimario.add(s.getCanal());
-				}
-
-			} else {
-				nuevoPrimario.add(auxiliar.get(j));
 			}
+
+			/*
+			 * Cargar Nuevo Secundario: Realizar algoritmo Shortest Path
+			 * Disjktra (SPD) desde el Nodo inicio al Nodo fin.
+			 */
+			Nodo nodoA = s1.getSolicitudes().get(i).getOrigen();
+			Nodo nodoB = s1.getSolicitudes().get(i).getDestino();
+			Camino subCamino = nodoA.dijkstra(nodoB);
+			// Se agrega el camino encontrado al Nuevo Camino Primario
+			for (Salto salto : subCamino.getSaltos()) {
+				nuevoSecundario.add(salto.getCanal());
+			}
+			Gen nuevoGen = new Gen(nuevoPrimario, nuevoSecundario);
+			/*
+			 * Cargar Genes al hijo.
+			 */
+			hijo.getGenes().add(i, nuevoGen);
 		}
 		return hijo;
 	}
